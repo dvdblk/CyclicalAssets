4b7689fd86bcfbb52dbb2e36546c22ff71161791
dvdblk <david.bielik1@gmail.com>
2016-01-13 12:18:27 +0100

    Initial
    Strong reference cycles intro

---
+++ CyclicalAssets/Accountant.swift
@@ -0,0 +1,9 @@
+//
+//  Accountant.swift
+//  CyclicalAssets
+//
+//  Created by David on 13/01/2016.
+//  Copyright © 2016 David. All rights reserved.
+//
+
+import Foundation
--- CyclicalAssets/main.swift
+++ CyclicalAssets/main.swift
@@ -8,5 +8,20 @@
 
 import Foundation
 
-print("Hello, World!")
+var bob: Person? = Person(name: "Bob")
+print("Created \(bob)")
 
+var laptop: Asset? = Asset(name: "Shiny laptop", value: 1500.0)
+var hat: Asset? = Asset(name: "Cowboy Hat", value: 175.0)
+var backpack: Asset? = Asset(name: "Blue backpack", value: 45.0)
+
+bob?.takeOwnershipOfAsset(laptop!)
+bob?.takeOwnershipOfAsset(backpack!)
+print("While bob's alive the owner of laptop is: \(laptop!.owner)")
+bob = nil
+print("the bob variable is now \(bob)")
+print("When bob's dead the owner of laptop is: \(laptop!.owner)")
+
+laptop = nil
+hat = nil
+backpack = nil
