fc01fb90633b80e16f23bdd0fc7e4ff9dd0fc00b
dvdblk <david.bielik1@gmail.com>
2016-01-13 15:10:59 +0100

    Added accountant class

--- CyclicalAssets/Accountant.swift
+++ CyclicalAssets/Accountant.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+class Accountant {
+    typealias NetWorthChanged = (Double) -> ()
+    
+    var netWorthChangedHandler: NetWorthChanged? = nil
+    
+    var netWorth: Double = 0.0 {
+        didSet {
+            netWorthChangedHandler?(netWorth)
+        }
+    }
+    
+    func gainedNewAsset(asset: Asset) {
+        netWorth += asset.value
+    }
+}
