c2edc610fe3981ba598bbd45268a9aee5b2a0e05
dave1337 <david.bielik1@gmail.com>
2016-01-13 10:20:37 +0100

    Initial Commit

---
+++ CyclicalAssets/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CyclicalAssets
+//
+//  Created by David on 13/01/2016.
+//  Copyright © 2016 David. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
